3d77b18558e18aa24ef0526c4851bfbf9dd701c9
yaldashbz <yaldashabanzadeh@gmail.com>
2023-08-23 16:32:23 +0000

    fix: fix warning & gt_masks in maskformer_model.py

--- mask2former/maskformer_model.py
+++ mask2former/maskformer_model.py
@@ -264,9 +264,10 @@ class MaskFormer(nn.Module):
     def prepare_targets(self, targets, images):
         h_pad, w_pad = images.tensor.shape[-2:]
         new_targets = []
-        for targets_per_image in targets:
+        for targets_per_image, image in zip(targets, images):
             # pad gt
             gt_masks = targets_per_image.gt_masks
+            gt_masks = BitMasks.from_polygon_masks(gt_masks, image.shape[1], image.shape[0]).tensor
             padded_masks = torch.zeros((gt_masks.shape[0], h_pad, w_pad), dtype=gt_masks.dtype, device=gt_masks.device)
             padded_masks[:, : gt_masks.shape[1], : gt_masks.shape[2]] = gt_masks
             new_targets.append(
